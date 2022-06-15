b123601411ec0616fa40e20379980b45be0cdecb
itt828 <iittt828@gmail.com>
2022-06-15 15:49:45 +0900

    :construction: questionpage

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/HomePage.vue";
+import QuestionPage from "../views/QuestionPage.vue";
 
-const routes = [{ path: "/", name: "Home", component: Home }];
+const routes = [
+  { path: "/", name: "Home", component: Home },
+  { path: "/question", name: "Question", component: QuestionPage },
+];
 
 const router = createRouter({
   history: createWebHistory(),
---
+++ src/views/QuestionPage.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="full-width column justify-center items-center">
+    <q-card class="mycard q-mb-md">
+      <div class="restaurant-name">四川屋台</div>
+      <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+
+      <q-card-section>
+        <div class="text-h6">ジャンル: ああ</div>
+        <div class="text-h6">価格帯: 100円</div>
+      </q-card-section>
+    </q-card>
+    <div class="full-width row justify-center">
+      <div class="col">
+        <q-btn round icon="close" size="lg" />
+      </div>
+      <div class="col">
+        <q-btn round icon="favorite" size="lg" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.restaurant-name {
+  font-size: 24pt;
+}
+
+.mycard {
+  max-width: 80%;
+  max-height: 100%;
+}
+</style>
