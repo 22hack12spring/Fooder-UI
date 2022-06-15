c37b2da033f38567b65ac95ad7e6c10756aa9e17
itt828 <iittt828@gmail.com>
2022-06-15 11:58:03 +0900

    :sparkles: add header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
 <template>
-  <router-view />
+  <q-layout view="hHh lpr fFf" class="shadow-2 rounded-borders">
+    <AppHeader />
+    <q-footer elevated>
+      <q-toolbar-title>Powered by ホットペッパー Webサービス</q-toolbar-title>
+    </q-footer>
+
+    <q-page-container>
+      <q-page class="q-pa-md">
+        <router-view />
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
+
+<script setup lang="ts">
+import AppHeader from "./components/AppHeader.vue";
+</script>
