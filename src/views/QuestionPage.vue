3c07dbe0f70d7116b16a5808fa83760b7fd396b9
itt828 <iittt828@gmail.com>
2022-06-17 00:23:39 +0900

    :zap: fix QuestionPage

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -6,15 +6,15 @@ import { ref } from "vue";
 import router from "../router";
 import { gourmetAnswerRequest } from "../apis";
 const store = useStore();
-// const questions: Array<GourmetQuestion> | null = store.state.questions;
+const questions: Array<GourmetQuestion> | null = store.state.questions;
 
-const question: GourmetQuestion = {
-  id: 1,
-  image: "image-url",
-  genre: "ジャンル",
-  subgenre: "サブジャンル",
-  price: "価格",
-};
+// const question: GourmetQuestion = {
+// id: 1,
+// image: "image-url",
+// genre: "ジャンル",
+// subgenre: "サブジャンル",
+// price: "価格",
+// };
 const questionLength = 7;
 
 // 便宜上1-indexedなので, 配列のindexとして利用するときに-1する
@@ -53,8 +53,11 @@ function allQuestionsAnswered() {
 </script>
 
 <template>
-  <div class="full-width column justify-center items-center">
-    <question-info-card :question="question" />
+  <div
+    v-if="questions !== null"
+    class="full-width column justify-center items-center"
+  >
+    <question-info-card :question="questions[currentQuestionNum - 1]" />
     <div class="full-width row justify-center">
       <div class="col">
         <q-btn round icon="close" size="lg" @click="onNoClicked" />
@@ -64,4 +67,5 @@ function allQuestionsAnswered() {
       </div>
     </div>
   </div>
+  <div v-else>質問の取得に失敗しました</div>
 </template>
