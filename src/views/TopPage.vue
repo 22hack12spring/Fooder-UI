ec90a0a34cf8cde43898bd1196ed77b1cc130cbf
keiichi11 <yokokei@icloud.com>
2022-06-16 23:37:06 +0900

    Update TopPage.vue
    修正しました

--- src/views/TopPage.vue
+++ src/views/TopPage.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { GourmetStartRequest } from "../apis/generated";
 import { gourmetStartRequest } from "../apis";
-
-const x: GourmetStartRequest = {
-  station: "text",
-};
 const text = ref("");
 async function Startclicked() {
-  await gourmetStartRequest(x);
+  await gourmetStartRequest({
+    station: text.value,
+  });
 }
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
