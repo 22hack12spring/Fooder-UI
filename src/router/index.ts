6b59ea8a7671f8d25332445103f98ba97d4dffac
itt828 <iittt828@gmail.com>
2022-06-17 01:43:31 +0900

    :zap: fix router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import QuestionPage from "../views/QuestionPage.vue";
 import Home from "../views/TopPage.vue";
+import ResultPage from "../views/ResultPage.vue";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/question", name: "Question", component: QuestionPage },
+  { path: "/result", name: "Result", component: ResultPage },
 ];
 
 const router = createRouter({
