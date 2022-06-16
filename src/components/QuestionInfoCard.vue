e4e14e10907eedd5e327dba1d8a86d36ba567a6a
itt828 <iittt828@gmail.com>
2022-06-17 01:08:50 +0900

    :lipstick: fix ui

--- src/components/QuestionInfoCard.vue
+++ src/components/QuestionInfoCard.vue
@@ -4,21 +4,26 @@ import { GourmetQuestion } from "../apis/generated";
 interface Props {
   question: GourmetQuestion | null;
 }
+// eslint-disable-next-line no-unused-vars
 const props = defineProps<Props>();
 </script>
 
 <template>
-  <q-card v-if="props.question !== null" class="mycard q-mb-md">
-    <div class="restaurant-name">{{ question?.id }}</div>
-    {{ question?.image }}
+  <q-card class="mycard q-mb-md full-width">
+    <div class="restaurant-name">Q{{ question?.id }}</div>
+    <q-img src="https://q.trap.jp/api/v3/public/icon/itt" />
     <q-card-section>
-      <div class="text-h6">
-        ジャンル: {{ question?.genre }}, サブジャンル: {{ question?.subgenre }}
+      <div class="row full-width">
+        <div class="col text-h6">
+          {{ question?.genre }}
+        </div>
+        <div class="col text-h6">
+          {{ question?.subgenre }}
+        </div>
       </div>
       <div class="text-h6">価格帯: {{ question?.price }}</div>
     </q-card-section>
   </q-card>
-  <div v-else>質問を取得できませんでした</div>
 </template>
 
 <style>
