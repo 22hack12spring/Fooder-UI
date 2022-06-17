a6bbdfd6ac4c5bad0a05dfc97fdd3bd52d61f524
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-06-17 22:40:52 +0900

    フッターを固定できた

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,31 @@
 <template>
-  <q-layout view="hHh lpr fFf" class="shadow-2 rounded-borders note-sans">
-    <AppHeader />
+  <q-layout view="hHh lpr fff" class="shadow-2 rounded-borders note-sans">
+    <AppHeaderVue />
+    <q-footer class="transparent">
+      <q-toolbar-title class="text-secondary footer">
+        Powered by
+        <a href="http://webservice.recruit.co.jp/" class="text-secondary link">
+          ホットペッパー Webサービス
+        </a></q-toolbar-title
+      >
+    </q-footer>
 
     <q-page-container>
-      <q-page class="q-pa-md container">
+      <q-page class="q-pa-md">
         <router-view />
-        <q-toolbar-title class="text-secondary footer">
-          Powered by
-          <a
-            href="http://webservice.recruit.co.jp/"
-            class="text-secondary link"
-          >
-            ホットペッパー Webサービス
-          </a></q-toolbar-title
-        >
       </q-page>
     </q-page-container>
   </q-layout>
 </template>
 
 <script setup lang="ts">
-import AppHeader from "./components/AppHeader.vue";
+import AppHeaderVue from "./components/AppHeader.vue";
 </script>
 
 <style scoped>
-.container {
-  position: relative;
-  padding-bottom: 20px;
-}
 .footer {
-  /* TODO: いい感じの書き方求む */
-  position: absolute;
-  bottom: 0px;
-  left: 50%;
-  transform: translateX(-50%);
   font-size: 12px;
-  padding: 4px;
 }
-
 .link {
   text-decoration: none;
 }
