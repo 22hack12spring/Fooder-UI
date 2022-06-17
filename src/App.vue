042fbb5d313ebe7ed4e498e21a1c40e51ba03972
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-06-17 22:22:41 +0900

    フッターの位置調整（うまく行かず）

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <q-layout view="hHh lpr fFf" class="shadow-2 rounded-borders note-sans">
     <AppHeader />
-    <q-footer elevated>
-      <q-toolbar-title>Powered by ホットペッパー Webサービス</q-toolbar-title>
-    </q-footer>
 
     <q-page-container>
-      <q-page class="q-pa-md">
+      <q-page class="q-pa-md container">
         <router-view />
+        <q-toolbar-title class="text-secondary footer">
+          Powered by
+          <a
+            href="http://webservice.recruit.co.jp/"
+            class="text-secondary link"
+          >
+            ホットペッパー Webサービス
+          </a></q-toolbar-title
+        >
       </q-page>
     </q-page-container>
   </q-layout>
@@ -16,3 +22,23 @@
 <script setup lang="ts">
 import AppHeader from "./components/AppHeader.vue";
 </script>
+
+<style scoped>
+.container {
+  position: relative;
+  padding-bottom: 20px;
+}
+.footer {
+  /* TODO: いい感じの書き方求む */
+  position: absolute;
+  bottom: 0px;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 12px;
+  padding: 4px;
+}
+
+.link {
+  text-decoration: none;
+}
+</style>
