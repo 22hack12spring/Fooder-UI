451725c990d6ea5b76387dd52c75d68c486b19eb
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-06-19 03:50:39 +0900

    notfoundページを少しリッチに

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,13 +4,15 @@ import Home from "../views/TopPage.vue";
 import ResultPage from "../views/ResultPage.vue";
 import ConceptPage from "../views/ConceptPage.vue";
 import ShopNotFound from "../views/ShopNotFound.vue";
+import PageNotFound from "../views/PageNotFound.vue";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/question", name: "Question", component: QuestionPage },
   { path: "/result", name: "Result", component: ResultPage },
   { path: "/concept", name: "Concept", component: ConceptPage },
-  { path: "/notfound", name: "NotFound", component: ShopNotFound },
+  { path: "/notfound", name: "ShopNotFound", component: ShopNotFound },
+  { path: "/:pathMatch(.*)*", name: "404", component: PageNotFound },
 ];
 
 const router = createRouter({
