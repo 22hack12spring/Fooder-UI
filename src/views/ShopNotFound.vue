b0e660ccc49b22266d2c08229efa8264277e20d2
itt828 <iittt828@gmail.com>
2022-06-18 22:48:31 +0900

    :zap: improve comment

--- src/views/ShopNotFound.vue
+++ src/views/ShopNotFound.vue
@@ -1,1 +1,10 @@
-<template>お店が見つかりませんでした(´・ω・`)</template>
+<template>
+  <div class="q-mt-xl">
+    <div class="q-mb-md text-secondary">
+      お店が見つかりませんでした(´・ω・`)
+    </div>
+    <div class="text-secondary">
+      検索する地名・駅名を<br />変える必要があるかもしれません
+    </div>
+  </div>
+</template>
