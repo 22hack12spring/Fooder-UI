49a65c2406e42ff7950b71b1f916f49662651c7f
itt828 <iittt828@gmail.com>
2022-06-18 10:25:54 +0900

    :zap: fix store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,24 @@
 import { InjectionKey } from "vue";
 import { createStore, Store, useStore as baseUseStore } from "vuex";
-import { GourmetQuestion } from "../apis/generated";
+import { GourmetQuestion, GourmetShop } from "../apis/generated";
 
 interface State {
   questions: Array<GourmetQuestion> | null;
+  result: GourmetShop | null;
 }
 export const key: InjectionKey<Store<State>> = Symbol();
 
 export const store = createStore<State>({
   state: {
     questions: null,
+    result: null,
   },
   mutations: {
     setQuestions(state, questions: Array<GourmetQuestion>) {
       state.questions = questions;
+    },
+    setResult(state, result: GourmetShop) {
+      state.result = result;
     },
   },
 });
